e05572b5b4155daf12a00d49898338bb1c43fa56
gfdgdfgdfgd <33372034+hiair2323@users.noreply.github.com>
2020-10-31 22:34:33 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/gomail.v2"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/email/:sender/:title/:dou", func(c *gin.Context) {
+		title := c.Param("title")
+		sender := c.Param("sender")
+		dou := c.Param("dou")
+		msg := gomail.NewMessage()
+		msg.SetHeader("To", sender)
+		msg.SetHeader("Subject", title)
+		msg.SetBody("text/plain", "您在专业研报终端-研报客的会员，还剩"+dou+"天将到期，豆子与推送到期将清零！请及时续费！")
+		msg.Attach("/data/" + title)
+		msg.SetAddressHeader("From", "mail@sendfile.yanbaoke.net", "研报客-全球研报速达")
+		d := gomail.NewPlainDialer("192.168.1.20", 657, "mail", "360tHKWznZCY4Fy3")
+		d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+		if err := d.DialAndSend(msg); err != nil {
+			fmt.Println(err)
+		}
+
+	})
+	router.Run(":8000")
+}
